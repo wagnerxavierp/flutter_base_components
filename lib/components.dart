0302f2f66c672c3d11df90fe977490aff6c313ab
Wagner Xavier <wagnerwxp@gmail.com>
2025-04-12 16:22:59 -0300

    ajustes

--- lib/components.dart
+++ lib/components.dart
@@ -2,6 +2,7 @@ library flutter_base_components;
 
 export 'src/components/list/list_component_controller.dart';
 export 'src/components/list/list_component.dart';
+export 'src/components/list/list_filter.dart';
 export 'src/components/list/card_widget.dart';
 export 'src/components/list/card_model_widget.dart';
 export 'src/components/layout/wrap_responsive.dart';
